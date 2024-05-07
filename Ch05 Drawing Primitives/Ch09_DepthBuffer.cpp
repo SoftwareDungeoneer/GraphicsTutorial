aca63071a89d31ae0cbc0d7ba2cb9cd7c1880b96
Software Dungeoneer <softwaredungeoneer@gmail.com>
2024-05-07 12:18:47 -0500

    Start on depth buffer demo

---
+++ Ch05 Drawing Primitives/Ch09_DepthBuffer.cpp
@@ -0,0 +1,83 @@
+#include "Ch09_DepthBuffer.h"
+
+#include <tchar.h>
+
+#include "util.h"
+
+void DepthBuffer::Initialize()
+{
+	auto vsBytes = LoadFile(_T("Ch09_Depth2D.vsc"));
+	auto psBytes = LoadFile(_T("CH09_Depth2D.psc"));
+	auto fullscreenVsBytes = LoadFile(_T("FullscreenQuad.vsc"));
+	auto fullscreenPsBytes = LoadFile(_T("TexturedQuad.psc"));
+
+	pDevice->CreateVertexShader(vsBytes.data(), vsBytes.size(), nullptr, &*vertexShader);
+	pDevice->CreateVertexShader(fullscreenVsBytes.data(), fullscreenVsBytes.size(), nullptr, &*fullscreenVS);
+	pDevice->CreatePixelShader(psBytes.data(), psBytes.size(), nullptr, &*pixelShader);
+	pDevice->CreatePixelShader(fullscreenPsBytes.data(), fullscreenPsBytes.size(), nullptr, &*fullscreenPS);
+
+	CreateTextureRenderTarget();
+}
+
+void DepthBuffer::CreateTextureRenderTarget()
+{
+	D3D11_TEXTURE2D_DESC texDesc;
+	texDesc.Width = 1024;
+	texDesc.Height = 768;
+	texDesc.MipLevels = 1;
+	texDesc.ArraySize = 1;
+	texDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	texDesc.SampleDesc = { 1, 0 };
+	texDesc.Usage = D3D11_USAGE_DEFAULT;
+	texDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET;
+	texDesc.CPUAccessFlags = 0;
+	texDesc.MiscFlags = 0;
+
+	pDevice->CreateTexture2D(&texDesc, nullptr, &*renderTexture);
+
+	// Create the shader resource view
+	D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc;
+	ZeroInitialize(srvDesc);
+	srvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+	srvDesc.Texture2D = { 1, 0 };
+	pDevice->CreateShaderResourceView(*renderTexture, &srvDesc, &*renderTextureSRV);
+
+	D3D11_RENDER_TARGET_VIEW_DESC rtvDesc;
+	ZeroInitialize(rtvDesc);
+	rtvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	rtvDesc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
+	rtvDesc.Texture2D.MipSlice = 0;
+	pDevice->CreateRenderTargetView(*renderTexture, &rtvDesc, &*renderTargetView);
+}
+
+void DepthBuffer::Update(double elapsed)
+{
+	if (!enableUpdate)
+		Initialize();
+}
+
+void DepthBuffer::Render()
+{
+	pDeviceContext->ClearState();
+
+	constexpr float clearColor[4]{ 1.f, 0.f, 1.f, 1.f };
+	pDeviceContext->ClearRenderTargetView(*renderTargetView, clearColor);
+
+	// Do the full screen copy
+	pDeviceContext->VSSetShader(*fullscreenVS, nullptr, 0);
+	pDeviceContext->PSSetShader(*fullscreenPS, nullptr, 0);
+	pDeviceContext->PSSetShaderResources(0, 1, &*renderTextureSRV);
+
+	ComPtr<ID3D11Texture2D> pBackBuffer;
+	ComPtr<ID3D11RenderTargetView> pRenderTarget;
+	pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&*pBackBuffer);
+	pDevice->CreateRenderTargetView(*pBackBuffer, nullptr, &*pRenderTarget);
+	D3D11_VIEWPORT viewport{ ViewportFromTexture(pBackBuffer) };
+	
+	pDeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
+	pDeviceContext->RSSetViewports(1, &viewport);
+	pDeviceContext->OMSetRenderTargets(1, &*pRenderTarget, nullptr);
+
+	pDeviceContext->Draw(4, 0);
+}
---
+++ Ch05 Drawing Primitives/Ch09_DepthBuffer.h
@@ -0,0 +1,53 @@
+#ifndef GRAPHICS_TUTORIAL_CH09_DEPTH_BUFFER_H
+#define GRAPHICS_TUTORIAL_CH09_DEPTH_BUFFER_H
+#pragma once
+
+#include "Renderer.h"
+
+#include <d3d11.h>
+
+#include <array>
+
+#include "ComPtr.h"
+
+class DepthBuffer : public Renderer
+{
+public:
+	DepthBuffer(HWND h) : Renderer(h) {}
+
+	virtual void Update(double elapsed);
+	virtual void Render();
+
+	struct Vertex
+	{
+		float Pos[3]{ 0, 0, 0 };
+		float Texcoord[2]{ 0, 0 };
+
+		static const D3D11_INPUT_ELEMENT_DESC desc[];
+	};
+
+protected:
+	virtual void Initialize();
+
+private:
+	void CreateTextureRenderTarget();
+
+	ComPtr<ID3D11VertexShader> vertexShader;
+	ComPtr<ID3D11PixelShader> pixelShader;
+	ComPtr<ID3D11InputLayout> inputLayout;
+
+	ComPtr<ID3D11VertexShader> fullscreenVS;
+	ComPtr<ID3D11PixelShader> fullscreenPS;
+
+	ComPtr<ID3D11Texture2D> renderTexture;
+	ComPtr<ID3D11RenderTargetView> renderTargetView;
+	ComPtr<ID3D11ShaderResourceView> renderTextureSRV;
+
+	ComPtr<ID3D11Texture2D> depthTexture;
+	ComPtr<ID3D11DepthStencilView> depthStencilView;
+	ComPtr<ID3D11DepthStencilState> depthStencilState;
+
+	std::array<Vertex, 9> verts;
+};
+
+#endif // GRAPHICS_TUTORIAL_CH09_DEPTH_BUFFER_H
